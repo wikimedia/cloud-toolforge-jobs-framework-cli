668eff3b0cceb790ab6eb4eb88b51b0fd7fa2dcf
Taavi Väänänen <hi@taavi.wtf>
2023-10-05 12:15:59 +0300

    d/changelog: prepare for new release 13
    Change-Id: I3da3e0cd3b4c0695f087d4c2956f904ff0c0b400

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="toolforge-jobs-framework-cli",
-    version="8",
+    version="13",
     author="Arturo Borrero Gonzalez",
     author_email="aborrero@wikimedia.org",
     license="GPL-3.0-or-later",
